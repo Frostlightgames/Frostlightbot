5870829eb118a22481120efba225b79335dd9630
gamexdifficulty <gamexdifficulty@yahoo.com>
2025-10-24 23:45:53 +0200

    Created  new member interface

--- data/classes/member.py
+++ data/classes/member.py
@@ -1,55 +1,47 @@
-class MemberManager:
-    def __init__(self,bot):
-        self.bot = bot
-        self.member_list = []
-
-    async def check(self):
-        
-        # Checks all discord guild member
-        for member in self.bot.get_all_members():
-            self.load(member)
-
-    def find(self,discord_member):
-        found = False
-        for member in self.member_list:
-
-            # Finds member by their id
-            if member.id == discord_member.id:
-                found = True
-                return member
-
-        if not found:
-
-            # No member found, creating a new one
-            new_member = Member(self.bot,discord_member.id,discord_member.name)
-            self.member_list.append(new_member)
-            return new_member
-
-    def save(self,discord_member):
-
-        # Writing current member data to database
-        member = self.find(discord_member)
-        self.bot.database.save_member(member)
-
-    def load(self,discord_member):
-
-        # Getting data from database or creating a new member
-        member = self.find(discord_member)
-        data = self.bot.database.load_member(member)
-        member.name = data[0]
-        member.id = data[1]
-        member.coins = data[2]
-        member.candy = data[3]
-        member.level = data[4]
-        member.xp = data[5]
-        return member
-
+from data.classes.database import DATABASE
 class Member:
-    def __init__(self,bot,id=0,name=""):
-        self.bot = bot
-        self.id = id
+    def __init__(self, uid, name):
+        self.uid = uid
         self.name = name
-        self.coins = 0
-        self.candy = 0
-        self.level = 0
-        self.xp = 0
+        self.coins = DATABASE.get_member_value(uid,"coins") or 0
+        self.candy = DATABASE.get_member_value(uid,"candy") or 0
+        self.level = DATABASE.get_member_value(uid,"level") or 0
+        self.xp = DATABASE.get_member_value(uid,"xp") or 0
+
+        DATABASE.set_member_value(uid,"name", self.name)
+
+    @property
+    def coins(self):
+        coins = DATABASE.get_member_value(self.uid, "coins")
+        return coins
+    
+    @coins.setter
+    def coins(self, value):
+        DATABASE.set_member_value(self.uid,"coins",value)
+
+    @property
+    def candy(self):
+        candy = DATABASE.get_member_value(self.uid, "candy")
+        return candy
+    
+    @candy.setter
+    def candy(self, value):
+        DATABASE.set_member_value(self.uid,"candy",value)
+
+    @property
+    def level(self):
+        level = DATABASE.get_member_value(self.uid, "level")
+        return level
+    
+    @level.setter
+    def level(self, value):
+        DATABASE.set_member_value(self.uid,"level",value)
+    
+    @property
+    def xp(self):
+        xp = DATABASE.get_member_value(self.uid, "xp")
+        return xp
+    
+    @xp.setter
+    def xp(self, value):
+        DATABASE.set_member_value(self.uid,"xp",value)
